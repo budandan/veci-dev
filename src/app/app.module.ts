d968e90f9e96d5a379f2a45bbd2aa274d18877f3
Daniel Ferolino <danielrferolino@gmail.com>
2017-07-16 16:55:37 -0600

    adds home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -11,16 +13,20 @@ import { CategoriesComponent } from './core/categories/categories.component';
 import { AboutUsComponent } from './core/about-us/about-us.component';
 import { MyProfileComponent } from './core/my-profile/my-profile.component';
 import { CartComponent } from './core/cart/cart.component';
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { LendComponent } from './core/lend/lend.component';
 
-
-const appRoutes = [
+const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'categories', component: CategoriesComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'profile', component: MyProfileComponent },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,11 +37,17 @@ const appRoutes = [
     CategoriesComponent,
     AboutUsComponent,
     MyProfileComponent,
-    CartComponent
+    CartComponent,
+    PageNotFoundComponent,
+    LendComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
